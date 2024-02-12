fe628788e3b9a82d25baa5620712ea7b8299315f
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-02-13 01:10:11 +0800

    commit trial ithink right

--- includes/trial/events.js
+++ includes/trial/events.js
@@ -28,36 +28,3 @@ AFRAME.registerComponent('markerhandler', {
             }
         });
 }});
-
-/*
-AFRAME.registerComponent('markerhandler', {
-
-    init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntitya = document.querySelector("#animated-model-1");
-        const aEntity2 = document.querySelector("#animated-model-2");
-        const aEntity3 = document.querySelector("#animated-model-3");
-
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntitya && intersectedElement === aEntitya) {
-                window.location.href = 'red.html';
-            }
-        });
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntityb && intersectedElement === aEntityb) {
-                window.location.href = 'green.html';
-            }
-        });
-        // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntityc && intersectedElement === aEntityc) {
-                window.location.href = 'blue.html';
-            }
-        });
-}});
-*/
