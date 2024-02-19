f1f924e0aefba46a53ef39a22451efa2a147bf0c
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-02-20 00:15:16 +0800

    commit added book info in scanning

--- includes/trial/events.js
+++ includes/trial/events.js
@@ -5,28 +5,26 @@ AFRAME.registerComponent('markerhandler', {
         const aEntityb = document.querySelector("#animated-model2");
         const aEntityc = document.querySelector("#animated-model3");
 
-        // Function to handle click events on the animated models
-        function handleClick(ev) {
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                window.location.href = 'red.html';
-            } else if (aEntityb && intersectedElement === aEntityb) {
-                window.location.href = 'green.html';
-            } else if (aEntityc && intersectedElement === aEntityc) {
-                window.location.href = 'blue.html';
-            }
-        }
+        // Add event listener for markerFound event
+        this.el.addEventListener('markerFound', function() {
+            // Get reference to the animated marker
+            const animatedMarker = document.querySelector("#animated-marker");
 
-        // Attach click event listener to the animated models
-        if (aEntity) {
-            aEntity.addEventListener('click', handleClick);
-        }
-        if (aEntityb) {
-            aEntityb.addEventListener('click', handleClick);
-        }
-        if (aEntityc) {
-            aEntityc.addEventListener('click', handleClick);
-        }
+            // Add click event listener to the animated marker
+            animatedMarker.addEventListener('click', function(ev) {
+                const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+                if (aEntity && intersectedElement === aEntity) {
+                    window.location.href = 'red.html';
+                } else if (aEntityb && intersectedElement === aEntityb) {
+                    window.location.href = 'green.html';
+                } else if (aEntityc && intersectedElement === aEntityc) {
+                    // Get the value attribute of the found marker
+                    const markerValue = animatedMarker.getAttribute('value');
+                    // Redirect to book_details.php with marker value appended as a query parameter
+                    window.location.href = '../../book_details.php?id=' + markerValue;
+                }
+            });
+        });
     }
 });
 
