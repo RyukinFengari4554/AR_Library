3e4791e3dbeedaa0d153717e21180c4f40007ace
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-03-06 19:16:35 +0800

    finished adding the redirection as well as error message

--- add_book.php
+++ add_book.php
@@ -13,22 +13,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $year = $_POST['year'];
     $genre = $_POST['genre'];
     $availability = $_POST['availability'];
+    try {
+        // SQL query to insert data into the 'books' table
+        $sql = "INSERT INTO ar_library.books (title, isbn_issn, call_num, shelf_loc, description, author, year, genre, availability) 
+                VALUES ('$title', '$isbn_issn', '$call_num', '$shelf', '$description', '$author', '$year', '$genre', '$availability')";
 
-    // SQL query to insert data into the 'books' table
-    $sql = "INSERT INTO ar_library.books (title, isbn_issn, call_num, shelf_loc, description, author, year, genre, availability) 
-            VALUES ('$title', '$isbn_issn', '$call_num', '$shelf', '$description', '$author', '$year', '$genre', '$availability')";
-
-    // Execute the SQL query
-    if ($mysqli->query($sql) === TRUE) {
-        echo "Book added successfully";
-        header("Location: admin.php");
-    exit();
-    } else {
-        echo "Error: " . $sql . "<br>" . $mysqli->error;
+        // Execute the SQL query
+        if ($mysqli->query($sql) === TRUE) {
+            echo "Book successfully added.";
+        } else {
+            throw new Exception("Error: " . $mysqli->error);
+        }
+    } catch (mysqli_sql_exception $exception) {
+        // Check if the error is due to a duplicate entry
+        if ($exception->getCode() == 1062) {
+            echo "Due to the same ISBN or ISSN, the book addition attempt was unsuccessful.";
+        } else {
+            echo "Error: " . $exception->getMessage();
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    } finally {
+        // Close the database connection
+        $mysqli->close();
     }
 } else {
     // Redirect to a different page if accessed directly
-    header("Location: admin.php");
+    header("Location: index.php");
     exit();
 }
 ?>
