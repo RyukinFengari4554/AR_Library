54a142d9776dea8686940472637c85cc22341eb2
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-03-13 19:18:10 +0800

    try for agir

--- includes/trial/events.js
+++ includes/trial/events.js
@@ -1,41 +1,51 @@
+ // Function to create model entities
+ function createModel(id, assetSrc, position, rotation) {
+    const entity = document.createElement('a-entity');
+    entity.setAttribute('id', id);
+    entity.setAttribute('gltf-model', assetSrc);
+    entity.setAttribute('scale', '50 50 50');
+    entity.setAttribute('rotation', rotation);
+    entity.setAttribute('position', position);
+    return entity;
+}
+
+// Function to create model entities for a marker
+function createModelsForMarker(markerId, asset1, asset2, asset3) {
+    const marker = document.getElementById(markerId);
+    marker.appendChild(createModel('animated-model', asset1, '0 0 0', '0 -90 0'));
+    marker.appendChild(createModel('animated-model2', asset2, '850 0 0', '0 -90 0'));
+    marker.appendChild(createModel('animated-model3', asset3, '400 0 -400', '0 -90 0'));
+}
 
 AFRAME.registerComponent('markerhandler', {
     init: function() {
-        // Add event listener for markerFound event for all markers
         const animatedMarkers = document.querySelectorAll(".animated-marker");
         animatedMarkers.forEach(function(animatedMarker) {
-            console.log("Marker ID:", animatedMarker.id); // Log the marker ID
             animatedMarker.addEventListener('click', function(ev) {
                 const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
                 if (intersectedElement) {
-                console.log("Intersected Element:", intersectedElement.id); // Log the intersected element ID
-                // Get references to the animated models within this marker
-                const aEntity = animatedMarker.querySelector("#animated-model"); //book loc
-                const aEntityb = animatedMarker.querySelector("#animated-model2"); //similar books
-                const aEntityc = animatedMarker.querySelector("#animated-model3"); //book info
-                if (aEntity && intersectedElement === aEntity) {
+                    const aEntity = animatedMarker.querySelector("#animated-model");
+                    const aEntityb = animatedMarker.querySelector("#animated-model2");
+                    const aEntityc = animatedMarker.querySelector("#animated-model3 ");
                     const markerValue = animatedMarker.getAttribute('value');
-                    // Redirect to similar_books.php with marker value appended as a query parameter
-                    //window.location.href = 'red.html?id='+ markerValue;
-                    console.log("markerValue:", markerValue);
-                } else if (aEntityb && intersectedElement === aEntityb) {
-                    // Get the value attribute of the found marker
-                    const markerValue = animatedMarker.getAttribute('value');
-                    // Redirect to similar_books.php with marker value appended as a query parameter
-                    window.location.href = '../../similar_books.php?id=' + markerValue;
-                } else if (aEntityc && intersectedElement === aEntityc) {
-                    // Get the value attribute of the found marker
-                    const markerValue = animatedMarker.getAttribute('value');
-                    // Redirect to book_details.php with marker value appended as a query parameter
-                    window.location.href = '../../book_details.php?id=' + markerValue;
-                }
-                }else{
+                    if (aEntity && intersectedElement === aEntity) {//loc
+                        console.log("Redirect to red.html?id=" + markerValue);
+                        window.location.href = 'red.html?id=' + markerValue;
+                    } else if (aEntityb && intersectedElement === aEntityb) {// similar
+                        console.log("Redirect to similar_books.php?id=" + markerValue);
+                        window.location.href = '../../similar_books.php?id=' + markerValue;
+                    } else if (aEntityc && intersectedElement === aEntityc) {//info
+                        console.log("Redirect to book_details.php?id=" + markerValue);
+                        window.location.href = '../../book_details.php?id=' + markerValue;
+                    }
+                } else {
                     console.log("Intersected element is undefined");
                 }
             });
         });
     }
 });
+
 
 
 
---
+++ includes/trial/woevents.php
@@ -0,0 +1,160 @@
+<?php
+// Define the array of book URLs
+
+$nft_books = array(
+    "tsotw1",
+    "agir",
+    "atotc",
+    "tbt",
+    "ts",
+    "jc",
+    "tgsohi",
+    "aac",
+    "fsd",
+    "tmmm",
+    "dr",
+    "pot2",
+    "mtpas",
+    "sftw",
+    "ev",
+    "fm",
+    "sasriph",
+    "crit",
+    "cah",
+    "cacmom",
+    "caohati",
+    "taed",
+    "a",
+    "3cocs",
+    "ict",
+    "casiep",
+    "mbom",
+    "fosa",
+    "msaad",
+    "cs2e",
+    "eitac",
+    "dome",
+    "raam",
+    "ddc",
+    "ahftsomh",
+    "twbhcc",
+    "isic",
+    "acgifbsp",
+    "saphoit",
+    "lmibb",
+    "hi",
+    "unacn",
+    "tfim",
+    "tfmp",
+    "hdi",
+    "o",
+    "nfaca",
+    "msn",
+    "smab",
+    "cdd",
+    "tkam"
+);
+?>
+
+<script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1.3.0/dist/aframe-master.min.js"></script>
+<style>
+  .arjs-loader {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .arjs-loader div {
+    text-align: center;
+    font-size: 1.25em;
+    color: white;
+  }
+</style>
+
+
+<!-- Load AR.js NFT extension -->
+<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+<body style='margin : 0px; overflow: hidden;'>
+  <!-- Show loader until image descriptors are loaded -->
+  <div class="arjs-loader">
+    <div>Loading, please wait...</div>
+  </div>
+  <!-- AR scene -->
+  <a-scene vr-mode-ui='enabled: false;' renderer="logarithmicDepthBuffer: true; precision: medium;" embedded arjs='trackingMethod: best; sourceType: webcam; debugUIEnabled: true;'>
+    <!-- Camera -->
+    <a-entity id="camera" camera position="0 1.6 0"></a-entity>
+
+    <!-- NFT marker definition -->
+    <?php foreach ($nft_books as $key => $book): ?>
+    <a-nft type='nft' value='<?php echo $key + 1 ?>' 
+    url='https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/nft/<?php echo $book ?>' smooth='true' smoothCount='10' smoothTolerance='0.01' smoothThreshold='5'>
+        <!-- Entity displaying the 3D model -->
+        <a-entity id="model1" gltf-model='https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/similar%20books.glb' scale="200 200 200" position="700 300 0" event-set__mouseenter="scale: 25 25 25" event-set__mouseleave="scale: 20 20 20" cursor="rayOrigin: mouse">
+            <a-link href="../../similar_books.php?id=" title="Similar_Books" target="_blank"></a-link>
+        </a-entity>
+        <a-entity id="model2" gltf-model='https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/book%20location.glb' scale="200 200 200" position="300 300 0" event-set__mouseenter="scale: 25 25 25" event-set__mouseleave="scale: 20 20 20" cursor="rayOrigin: mouse">
+            <a-link href="red.html?id=" title="Book_Location" target="_blank"></a-link>
+        </a-entity>
+        <a-entity id="model3" gltf-model='https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/book%20information.glb' scale="200 200 200" position="550 300 -200" event-set__mouseenter="scale: 25 25 25" event-set__mouseleave="scale: 20 20 20" cursor="rayOrigin: mouse">
+            <a-link href="../../book_details.php?id=" title="Book_Details" target="_blank"></a-link>
+        </a-entity>
+    </a-nft>
+    <?php endforeach; ?>
+  </a-scene>
+</body>
+
+  <!-- COMMENTS FOR 3D MODELS
+    onmousedown="window.open('../../similar_books.php?id=', '_blank')" 
+    onmousedown="window.open('red.html', '_blank')"
+    onmousedown="window.open('../../book_details.php?id=', '_blank')"
+    pixels<1k scale="20 20 20" rotation="0 -90 0" , SB:  position="700 300 0" , BL: position="300 300 0", BD: position="550 300 -200" 
+    pixels=default rotation="0 -90 0" , SB:  position="700 300 0" , BL: position="300 300 0", BD: position="550 300 -200"  // not yet done
+  -->
+
+<script>
+
+document.addEventListener("DOMContentLoaded", function() {
+    var nfts = document.querySelectorAll("a-nft");
+    
+ 
+    nfts.forEach(function(nft, index) {
+        var nftValue = nft.getAttribute("value");
+        var models = nft.querySelectorAll("[id^='model']");
+        models.forEach(function(model) {
+            var link = model.querySelector("a-link");
+            if (link && nftValue) {
+                if (model.id === "model1") {
+                    link.setAttribute("href", "../../similar_books.php?id=" + nftValue);
+                } else if (model.id === "model2") {
+                    link.setAttribute("href", "red.html?id=" + nftValue);
+                } else if (model.id === "model3") {
+                    link.setAttribute("href", "../../book_details.php?id=" + nftValue);
+                }
+            }
+        });
+    });
+});
+
+
+
+
+
+    /*
+    document.addEventListener("DOMContentLoaded", function() {
+      var model1 = document.getElementById("model1");
+      var link1 = model1.querySelector("a-link");
+      var nftValue1 = document.querySelector("a-nft").getAttribute("value");
+      link1.setAttribute("href", "../../similar_books.php?id=" + nftValue1);
+      var model3 = document.getElementById("model3");
+      var link3 = model3.querySelector("a-link");
+      var nftValue3 = document.querySelector("a-nft").getAttribute("value");
+      link3.setAttribute("href", "../../book_details.php?id=" + nftValue3);
+    });
+   */
+  </script>
