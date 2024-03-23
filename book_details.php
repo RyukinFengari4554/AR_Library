465ee72be9439a91728c882f9d50206726786026
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-03-23 17:55:39 +0800

    including wayfind hyperlink in location

--- book_details.php
+++ book_details.php
@@ -35,6 +35,13 @@
     background-color: white;
     background-image:none;
     }
+    a {
+        color: black; 
+        text-decoration: none; 
+    } 
+    a:visited {
+        color: black; 
+    }
 </style>
 </head>
 <body>
@@ -87,7 +94,7 @@
                         echo "<i class='fa-solid fa-circle-xmark fa-lg' style='color: red;'></i></h2></td>"; 
                     }
                     echo "</table><table style='margin-left: 3rem; margin-right: 3rem;'></tr><tr><td><p><strong>Call Number:</strong> $book_cn</p></td></tr>"; 
-                    echo "<tr><td><p><strong>Shelf Location:</strong> $book_shelf</p></td></tr>"; //updated later
+                    echo "<tr><td><a href='map-all.php?id=" . $book_id . "'><p><strong>Shelf Location:</strong> $book_shelf <strong><i class='fa-solid fa-location-dot'></i></strong></p></a></td></tr>"; 
                     echo "<tr><td><h3 style='margin: 0;'>Plot Summary:</h3></td></tr><tr style='margin: 0; padding: 0;'><td><p style='margin: 0; padding: 0; text-align: justify;text-justify: inter-word;'> $book_desc</p></td></tr></table>";
                 } else {
                     echo "<h2 class='centeral'>Book not found</h2>";
