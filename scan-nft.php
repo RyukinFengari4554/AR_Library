de0655ca5a512827839d75337f399e3358997316
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-04-06 16:46:57 +0800

    width 300

--- scan-nft.php
+++ scan-nft.php
@@ -145,7 +145,7 @@ if(isset($_SESSION['my_array'])) {
                 id="animated-marker-<?php echo $id ?>"
                 type='nft'
                 url='includes/nft-books/<?php echo $marker ?>'
-                width='0.50'
+                width='300'
                 value='<?php echo $id ?>'
                 smooth='true' smoothCount='10' smoothTolerance='0.01' smoothThreshold='5'>
 
