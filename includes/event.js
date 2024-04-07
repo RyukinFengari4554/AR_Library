829b097d1d32b415f33c34e4b1be870bbf1a132d
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-04-07 23:37:17 +0800

    back to previous events

--- includes/event.js
+++ includes/event.js
@@ -1,53 +1,54 @@
 AFRAME.registerComponent('markerhandler', {
-  init: function() {
-    var self = this;
-
-    // Wait for the marker to be found
-    this.el.addEventListener('markerFound', function() {
-      var nfts = document.querySelectorAll("a-nft");
-
-      // Iterate over each a-nft element
-      nfts.forEach(function(nft) {
-        var nftValue = nft.getAttribute("value");
-        var models = nft.querySelectorAll("[id^='model']");
-
-        // Add click event listener to each a-nft element
-        nft.addEventListener('click', function(ev) {
-          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-          models.forEach(function(model) {
-            // Get the bounding box of the model, considering position and scale
-            var boundingBox = new THREE.Box3().setFromObject(model.object3D);
-            var modelPosition = model.getAttribute('position');
-            var modelScale = model.getAttribute('scale');
-
-            // Adjust the bounding box position based on the model's position and scale
-            boundingBox.min.add(new THREE.Vector3(modelPosition.x - modelScale.x / 2, modelPosition.y - modelScale.y / 2, modelPosition.z - modelScale.z / 2));
-            boundingBox.max.add(new THREE.Vector3(modelPosition.x + modelScale.x / 2, modelPosition.y + modelScale.y / 2, modelPosition.z + modelScale.z / 2));
-
-            // Check if the intersected element is within the clickable area boundaries
-            if (
-              intersectedElement === model &&
-              self.isWithinBounds(intersectedElement.object3D.position.x, boundingBox.min.x, boundingBox.max.x) &&
-              self.isWithinBounds(intersectedElement.object3D.position.y, boundingBox.min.y, boundingBox.max.y) &&
-              self.isWithinBounds(intersectedElement.object3D.position.z, boundingBox.min.z, boundingBox.max.z)
-            ) {
-              // Perform actions based on the clicked model
-              // You can replace the window.location.href with your desired action
-              if (model.id === "model1-" + nftValue) {
-                window.location.href = 'map-all.php?id=' + nftValue;
-              } else if (model.id === "model2-" + nftValue) {
-                window.location.href = "similar_books.php?id=" + nftValue;
-              } else if (model.id === "model3-" + nftValue) {
-                window.location.href = "book_details.php?id=" + nftValue;
+    init: function() {
+      var self = this;
+  
+      // Wait for the marker to be found
+      this.el.addEventListener('markerFound', function() {
+        var nfts = document.querySelectorAll("a-nft");
+  
+        // Iterate over each a-nft element
+        nfts.forEach(function(nft) {
+          var nftValue = nft.getAttribute("value");
+          var models = nft.querySelectorAll("[id^='model']");
+  
+          // Add click event listener to each a-nft element
+          nft.addEventListener('click', function(ev) {
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            models.forEach(function(model) {
+              // Define the clickable area using position and scale
+              var clickableAreaPosition = model.getAttribute('position');
+              var clickableAreaScale = model.getAttribute('scale');
+  
+              // Define the boundaries of the clickable area
+              var minX = clickableAreaPosition.x - clickableAreaScale.x / 2;
+              var maxX = clickableAreaPosition.x + clickableAreaScale.x / 2;
+              var minY = clickableAreaPosition.y - clickableAreaScale.y / 2;
+              var maxY = clickableAreaPosition.y + clickableAreaScale.y / 2;
+  
+              // Check if the intersected element is within the clickable area boundaries
+              if (
+                intersectedElement === model &&
+                self.isWithinBounds(intersectedElement.object3D.position.x, minX, maxX) &&
+                self.isWithinBounds(intersectedElement.object3D.position.y, minY, maxY)
+              ) {
+                // Perform actions based on the clicked model
+                // You can replace the window.location.href with your desired action
+                if (model.id === "model1-" + nftValue) {
+                  window.location.href = 'map-all.php?id=' + nftValue;
+                } else if (model.id === "model2-" + nftValue) {
+                  window.location.href = "similar_books.php?id=" + nftValue;
+                } else if (model.id === "model3-" + nftValue) {
+                  window.location.href = "book_details.php?id=" + nftValue;
+                }
               }
-            }
+            });
           });
         });
       });
-    });
-  },
-
-  isWithinBounds: function(value, min, max) {
-    return value >= min && value <= max;
-  }
-});
+    },
+  
+    isWithinBounds: function(value, min, max) {
+      return value >= min && value <= max;
+    }
+  });
+  
