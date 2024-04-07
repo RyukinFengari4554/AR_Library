7993591206e6a04f8cfc5d731b24fb6f08ad5c15
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-04-07 21:14:39 +0800

    try diff page default for scaniing nft

---
+++ scan-default.php
@@ -0,0 +1,189 @@
+<?php
+require_once "includes/db.inc.php";
+session_start();
+if(isset($_SESSION['my_array'])) {
+  // Retrieve the array from the session
+  $nft_books = $_SESSION['my_array'];
+
+  // Output the array to JavaScript console
+  echo '<script>';
+  echo 'console.log("NFT books:", ' . json_encode($nft_books) . ');';
+  echo '</script>';
+} else {
+  // Session variable doesn't exist
+  echo '<script>';
+  echo 'console.log("Session variable my_array does not exist.");';
+  echo '</script>';
+}
+
+?>
+<!doctype HTML>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    </head>
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1.3.0/dist/aframe-master.min.js"></script>
+<link href="styles/home.css" rel="stylesheet">
+<style>
+  .arjs-loader {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .arjs-loader div {
+    text-align: center;
+    font-size: 1.25em;
+    color: white;
+  }
+  .icon {
+      font-size: 2.5rem;
+      color: #333;
+      cursor: pointer;
+      transition: color 0.3s;
+    }
+
+.fixed-buttons {
+    position: fixed;
+    bottom: 5rem; /* Adjust the distance from the bottom */
+    left: 50%; /* Place buttons in the center horizontally */
+    transform: translateX(-50%); /* Center buttons horizontally */
+    z-index: 9999; /* Ensure buttons appear on top of other content */
+  }
+</style>
+
+
+<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+<script src="https://raw.githack.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+
+
+<script src="includes/event.js"></script>
+
+<body style='margin : 0px; overflow: hidden;'>
+<!--
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #dfbcbc; background-image: linear-gradient(white , #FFE3E5);">
+        
+        <a class="navbar-brand mb-0 h1" href="index.html"><img src="https://upload.wikimedia.org/wikipedia/en/4/42/Emilio_Aguinaldo_College_seal.svg" style="width:2rem;height:2rem;"> EAC AR Library</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+      
+            <li class="nav-item">
+              <a class="nav-link links" href="scan.php"><i class="fa-solid fa-book-open"></i> Scan a Book</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link links" href="catalogue.php"><i class="fa-solid fa-search"></i> Catalogue</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link links" href="map-whole.php"><i class="fa-solid fa-map"></i> Map</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link links" href="login-wsa.php"><i class="fa-solid fa-user"></i> Log-in</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
+        <table style="width:100%">
+          <tr>
+              <td style="width:25%"><img src="https://upload.wikimedia.org/wikipedia/en/4/42/Emilio_Aguinaldo_College_seal.svg" style="width:50%;height:50%;"> </td>
+              <td style="text-align: center;">
+                  <h1 style="color: red;">Emilio Aguinaldo College</h1>
+                  Gov. D. Mangubat Ave., Brgy. Burol Main, City of Dasmariñas, Cavite 4114, Philippines<br >
+                  Tel. Nos. (046) 416-4339/41<br>
+                  <a href="https://www.eac.edu.ph" target="_blank" rel="noopener noreferrer">www.eac.edu.ph</a><br>
+                  <br>
+                  <h3>School of Engineering and Technology</h3>
+              </td>
+          </tr>
+      </table>
+      -->
+<div class="fixed-buttons">
+<a href="index.html"><button style="font-size: medium;"><i class="fa-solid fa-house"></i></button></a>
+<a href="javascript:history.back()"><button style="font-size: medium;"><i class="fa-solid fa-arrow-left"></i></button></a>
+</div> 
+<div class="arjs-loader">
+    <div>Loading, please wait...</div>
+  </div>
+
+  <a-scene
+    id="myS"
+    vr-mode-ui='enabled: false;'
+    renderer="logarithmicDepthBuffer: true; precision: medium;"
+    embedded>
+
+    <a-assets>
+      <a-asset-item id="animated-asset" src="includes/book%20location.glb"></a-asset-item>
+    </a-assets>
+    <a-assets>
+      <a-asset-item id="animated-asset2" src="includes/similar%20books.glb"></a-asset-item>
+    </a-assets>
+    <a-assets>
+      <a-asset-item id="animated-asset3" src="includes/book%20information.glb"></a-asset-item>
+    </a-assets>
+  
+  <?php if (!empty($nft_books)): ?>
+    <?php foreach ($nft_books as $id => $marker): ?>
+    <a-nft 
+         markerhandler
+                emitevents="true"
+                cursor="rayOrigin: mouse"
+                id="animated-marker-<?php echo $id ?>"
+                type='nft'
+                url='includes/nft-books/<?php echo $marker ?>'
+                
+                value='<?php echo $id ?>'
+                smooth='true' smoothCount='10' smoothTolerance='0.01' smoothThreshold='5'>
+
+            <a-entity
+                    id="model1-<?php echo $id ?>" 
+                    gltf-model="#animated-asset"
+                    rotation="0 -90 0"
+                    scale="5 5 5"
+                    position="150 300 -100"> <!-- Book Location 3D model -->
+            </a-entity>
+            <a-entity
+                    id="model2-<?php echo $id ?>" 
+                    gltf-model="#animated-asset2"
+                    rotation="0 -90 0"
+                    scale="5 5 5"
+                    position="150 300 -100"> <!-- Similar Books 3D model -->
+            </a-entity>
+            <a-entity
+                    id="model3-<?php echo $id ?>" 
+                    gltf-model="#animated-asset3"
+                    rotation="0 -90 0"
+                    scale="5 5 5"
+                    position="150 300 -100"> <!-- Book Information 3D model -->
+            </a-entity>
+        </a-nft>
+
+    <?php endforeach; ?>
+
+    <a-entity camera></a-entity>
+    <?php endif; ?>
+
+  </a-scene>
+
+  <!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+<script src="https://kit.fontawesome.com/7dd0b53595.js" crossorigin="anonymous"></script>
+</html>
