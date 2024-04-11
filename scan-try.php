1bc5cdb88ea7b8159bf65b1389939cf3d668eafd
RyukinFengari4554 <reibun.morikyo@gmail.com>
2024-04-11 17:51:17 +0800

    scan-try

--- scan-try.php
+++ scan-try.php
@@ -59,86 +59,95 @@ window.addEventListener('load', function() {
       }
     });
   </script>
+ <a-scene
+      vr-mode-ui='enabled: false;'
+      renderer="logarithmicDepthBuffer: true; precision: medium;"
+      embedded
+      arjs='sourceType: webcam; debugUIEnabled: false;'
+      style="width: 100%; height: 100%;">
+      
+      <?php if (!empty($nft_books)): ?>
+    <?php foreach ($nft_books as $id => $marker): ?>
+    <!-- Define your NFT marker -->
+    <a-nft
+      type="nft"
+      id='nft-<?php echo $id ?>'
+      url='includes/nft-books/<?php echo $marker ?>'
+      value='<?php echo $id ?>'
+      smooth="true"
+      smoothCount="10"
+      smoothTolerance=".01"
+      smoothThreshold="5">
 
-  <a-scene
-    vr-mode-ui='enabled: false;'
-    renderer="logarithmicDepthBuffer: true; precision: medium;"
-    embedded
-    arjs='sourceType: webcam; debugUIEnabled: false;'
-    style="width: 100%; height: 100%;">
-
-    <?php if (!empty($nft_books)): ?>
-      <?php foreach ($nft_books as $id => $marker): ?>
-        <!-- Define your NFT marker -->
-        <a-nft
-          type="nft"
-          id='nft-<?php echo $id ?>'
-          url='https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/nft-books/<?php echo $marker ?>'
-          value='<?php echo $id ?>'
-          smooth="true"
-          smoothCount="10"
-          smoothTolerance=".01"
-          smoothThreshold="5">
-
-          <!-- Define the 3D model that will be displayed when the NFT marker is detected -->
-          <?php
+             <!-- Define the 3D model that will be displayed when the NFT marker is detected -->
+             <?php
           $position1 = '';
           $position2 = '';
           $position3 = '';
-
-          if ($id == 9 || ($id >= 21 && $id <= 30) || $id == 33) {
+          $scale = '20 20 20';
+          if ($id == 5 || $id == 7){
+            $position1 = '275 -120 -275';
+            $position2 = '363 -120 -200';
+            $position3 = '450 -120 -275';
+          } elseif ($id == 9 || ($id >= 21 && $id <= 30) || $id == 33) {
             $position1 = '105 -120 -225';
             $position2 = '193 -120 -150';
             $position3 = '280 -120 -225';
           } elseif ($id == 10) {
-            $position1 = '275 -140 -225';
-            $position2 = '363 -140 -150';
-            $position3 = '450 -140 -225';
-          } elseif ($id == 11 || $id == 13) {
+            $position1 = '275 -140 -275';
+            $position2 = '363 -140 -200';
+            $position3 = '450 -140 -275';
+          }elseif ($id == 11 || ($id >= 13 && $id <= 20)) {
+            /*
+            // scaled 20 
+            $id == 11 || $id == 13
             $position1 = '275 -150 -225';
             $position2 = '363 -150 -150';
             $position3 = '450 -150 -225';
+            */
+            $scale = '30 30 30';
+            $position1 = "412.5 -180 -337.5";
+            $position2 = "544.5 -180 -225";
+            $position3 = "675 -180 -337.5";
           } else {
             $position1 = '275 -120 -225';
             $position2 = '363 -120 -150';
             $position3 = '450 -120 -225';
           }
-
-        
           ?>
 
           <a-entity
-            gltf-model="url(https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/similar%20books.glb)"
-            scale="20 20 20"
+            gltf-model="url(includes/similar%20books.glb)"
+            scale="<?php echo $scale ?>"
             position="<?php echo $position1 ?>"
             rotation="0 -90 0"
             cursor-listener="href: similar_books.php?id=<?php echo $id ?>">
           </a-entity>
 
           <a-entity
-            gltf-model="url(https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/book%20information.glb)"
-            scale="20 20 20"
+            gltf-model="url(includes/book%20information.glb)"
+            scale="<?php echo $scale ?>"
             position="<?php echo $position2 ?>"
             rotation="0 -90 0"
             cursor-listener="href: book_details.php?id=<?php echo $id ?>">
           </a-entity>
 
           <a-entity
-            gltf-model="url(https://raw.githack.com/RyukinFengari4554/AR_Library/main/includes/book%20location.glb)"
-            scale="20 20 20"
+            gltf-model="url(includes/book%20location.glb)"
+            scale="<?php echo $scale ?>"
             position="<?php echo $position3 ?>"
             rotation="0 -90 0"
             cursor-listener="href: map-all.php?id=<?php echo $id ?>">
           </a-entity>
 
-        </a-nft>
-      <?php endforeach; ?>
-    <?php endif; ?>
-
-    <a-camera look-controls="enabled: false">
+    </a-nft>
+    <?php endforeach; ?>
+    <?php endif;?>
+    <!-- Camera setup -->
+    <a-camera rotation-reader  look-controls="enabled: false">
       <a-entity cursor="fuse: false;rayOrigin:mouse;" raycaster="objects:a-entity" position="0 0 -1" geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03" material="color: transparent; opacity: 0;shader: flat">
       </a-entity>
     </a-camera>
   </a-scene>
-  
+</div>
 </body>
